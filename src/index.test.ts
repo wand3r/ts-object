e1fe527601bba36d084fcf3285c9b94ab3d96907
Wojciech Karas <wkaras89@gmail.com>
2017-12-29 13:37:29 +0100

    map, filter, reduce, some, every, mapToArray implementation

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,116 @@
-import { add } from "./index"
+import { map, filter, reduce, some, every, mapToArray } from "./index"
 
-test("add", () => {
-  expect(add(1, 2)).toBe(3)
+describe("map", () => {
+  const obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+  }
+  it("with arity 2", () => {
+    const mappedObj = map((x, key) => (key === "b" ? x * 3 : x * 2), obj)
+    expect(mappedObj).toEqual({ a: 2, b: 6, c: 6 })
+  })
+  it("with arity 1", () => {
+    const mappedObj = map<typeof obj, number>(
+      (x, key) => (key === "b" ? x * 3 : x * 2),
+    )(obj)
+    expect(mappedObj).toEqual({ a: 2, b: 6, c: 6 })
+  })
+})
+
+describe("filter", () => {
+  const obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+  }
+  it("with arity 2", () => {
+    const filteredObj = filter((v, k) => v > 1 && k > "b", obj)
+    expect(filteredObj).toEqual({ c: 3 })
+  })
+  it("with arity 1", () => {
+    const filteredObj = filter<typeof obj>((v: number, k) => v > 1 && k > "b")(
+      obj,
+    )
+    expect(filteredObj).toEqual({ c: 3 })
+  })
+})
+
+describe("reduce", () => {
+  const obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+  }
+  const result = [{ v: 1, k: "a" }, { v: 2, k: "b" }, { v: 3, k: "c" }]
+  it("with arity 3", () => {
+    const reducedObj = reduce(
+      (acc, v, k) => [...acc, { v, k }],
+      [] as Array<{ v: number; k: string }>,
+      obj,
+    )
+    expect(reducedObj).toEqual(result)
+  })
+  it("with arity 2", () => {
+    const reducedObj = reduce<
+      typeof obj,
+      Array<{
+        v: number
+        k: string
+      }>
+    >((acc, v, k) => [...acc, { v, k }], [])(obj)
+    expect(reducedObj).toEqual(result)
+  })
+})
+
+describe("some", () => {
+  const obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+  }
+  it("with arity 2", () => {
+    expect(some((v, k) => v === 2 && k === "b", obj)).toBeTruthy()
+    expect(some((v, k) => v === 1 && k === "b", obj)).toBeFalsy()
+  })
+  it("with arity 1", () => {
+    expect(some<typeof obj>((v, k) => v === 2 && k === "b")(obj)).toBeTruthy()
+    expect(some<typeof obj>((v, k) => v === 1 && k === "b")(obj)).toBeFalsy()
+  })
+})
+
+describe("every", () => {
+  const obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+  }
+  it("with arity 2", () => {
+    expect(every((v, k) => v > 0, obj)).toBeTruthy()
+    expect(every((v, k) => v > 1, obj)).toBeFalsy()
+  })
+  it("with arity 1", () => {
+    expect(every<typeof obj>((v, k) => v > 0)(obj)).toBeTruthy()
+    expect(every<typeof obj>((v, k) => v > 1)(obj)).toBeFalsy()
+  })
+})
+
+describe("mapToArray", () => {
+  const obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+  }
+  const result = [{ v: 1, k: "a" }, { v: 2, k: "b" }, { v: 3, k: "c" }]
+  it("with arity 2", () => {
+    const arr = mapToArray((v, k) => ({ v, k }), obj)
+    expect(arr).toEqual(result)
+  })
+  it("with arity 1", () => {
+    const arr = mapToArray<typeof obj, { k: string; v: number }>((v, k) => ({
+      v,
+      k,
+    }))(obj)
+    expect(arr).toEqual(result)
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,153 @@
-export const add = (a: number, b: number) => a + b
+type Transform<V, K, R> = (value: V, key: K) => R
+type Predicate<V, K> = (value: V, key: K) => boolean
+type Reducer<Acc, V, K> = (acc: Acc, value: V, key: K) => Acc
 
-console.log(add(1, 2))
+type Arity2<A, B, C> = (a: A, b: B) => C
+type Curried2<A, B, C> = {
+  (a: A, b: B): C
+  (a: A): (b: B) => C
+}
+const curry2: <A, B, C>(fn: Arity2<A, B, C>) => Curried2<A, B, C> = (fn) => {
+  const curried = (...args: any[]) => {
+    switch (args.length) {
+      case 2: {
+        const [a, b] = args
+        return fn(a, b)
+      }
+      case 1: {
+        const [a] = args
+        return (b: any) => fn(a, b)
+      }
+      default:
+        throw new Error("Wrong arity")
+    }
+  }
+  return curried as Curried2<any, any, any>
+}
+
+type Arity3<A, B, C, D> = (a: A, b: B, c: C) => D
+type Curried3<A, B, C, D> = {
+  (a: A, b: B, c: C): D
+  (a: A, b: B): (c: C) => D
+}
+const curry3: <A, B, C, D>(fn: Arity3<A, B, C, D>) => Curried3<A, B, C, D> = (
+  fn,
+) => {
+  const curried = (...args: any[]) => {
+    switch (args.length) {
+      case 3: {
+        const [a, b, c] = args
+        return fn(a, b, c)
+      }
+      case 2: {
+        const [a, b] = args
+        return (c: any) => fn(a, b, c)
+      }
+      default:
+        throw new Error("Wrong arity")
+    }
+  }
+  return curried as Curried3<any, any, any, any>
+}
+
+type Map = {
+  <O, R, K extends keyof O = keyof O>(map: Transform<O[K], K, R>, obj: O): {
+    [P in K]: R
+  }
+  <O, R, K extends keyof O = keyof O>(map: Transform<O[K], K, R>): (
+    obj: O,
+  ) => Record<K, R>
+}
+export const map = curry2(
+  <O, R, K extends keyof O = keyof O>(
+    transform: Transform<O[K], K, R>,
+    obj: O,
+  ): Record<K, R> =>
+    Object.keys(obj).reduce(
+      (mappedObj, key: K) => {
+        mappedObj[key] = transform(obj[key], key)
+        return mappedObj
+      },
+      {} as Record<K, R>,
+    ),
+) as Map
+
+type Filter = {
+  <O, K extends keyof O = keyof O>(
+    predicate: Predicate<O[K], K>,
+    obj: O,
+  ): Partial<O>
+  <O, K extends keyof O = keyof O>(predicate: Predicate<O[K], K>): (
+    obj: O,
+  ) => Partial<O>
+}
+export const filter = curry2(
+  <O, K extends keyof O>(predicate: Predicate<O[K], K>, obj: O): Partial<O> => {
+    return Object.keys(obj).reduce(
+      (filteredObj, key: K) => {
+        if (predicate(obj[key], key)) filteredObj[key] = obj[key]
+        return filteredObj
+      },
+      {} as Partial<O>,
+    )
+  },
+) as Filter
+
+type Reduce = {
+  <O, Acc, K extends keyof O = keyof O>(
+    fn: Reducer<Acc, O[K], K>,
+    init: Acc,
+    obj: O,
+  ): Acc
+  <O, Acc, K extends keyof O = keyof O>(fn: Reducer<Acc, O[K], K>, init: Acc): (
+    obj: O,
+  ) => Acc
+}
+export const reduce = curry3(
+  <O, Acc, K extends keyof O = keyof O>(
+    fn: Reducer<Acc, O[K], K>,
+    init: Acc,
+    obj: O,
+  ): Acc =>
+    Object.keys(obj).reduce((acc, key: K) => fn(acc, obj[key], key), init),
+) as Reduce
+
+type Obj2Boolean = {
+  <O, K extends keyof O = keyof O>(
+    predicate: Predicate<O[K], K>,
+    obj: O,
+  ): boolean
+  <O, K extends keyof O = keyof O>(predicate: Predicate<O[K], K>): (
+    obj: O,
+  ) => boolean
+}
+export const some = curry2(
+  <O, K extends keyof O = keyof O>(
+    predicate: Predicate<O[K], K>,
+    obj: O,
+  ): boolean => Object.keys(obj).some((key: K) => predicate(obj[key], key)),
+) as Obj2Boolean
+
+export const every = curry2(
+  <O, K extends keyof O = keyof O>(
+    predicate: Predicate<O[K], K>,
+    obj: O,
+  ): boolean => Object.keys(obj).every((key: K) => predicate(obj[key], key)),
+) as Obj2Boolean
+
+type MapToArray = {
+  <O, R, K extends keyof O = keyof O>(pick: Transform<O[K], K, R>, obj: O): R[]
+  <O, R, K extends keyof O = keyof O>(pick: Transform<O[K], K, R>): (
+    obj: O,
+  ) => R[]
+}
+export const mapToArray = curry2(
+  <O, R, K extends keyof O = keyof O>(
+    pick: Transform<O[K], K, R>,
+    obj: O,
+  ): R[] =>
+    Object.keys(obj).reduce(
+      (arr, key: K) => [...arr, pick(obj[key], key)],
+      [] as R[],
+    ),
+) as MapToArray
